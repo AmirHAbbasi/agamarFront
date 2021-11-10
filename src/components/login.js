da8a13f02745bca26cf92bbb250744b7086acb67
Sahar Mehrabi <s_mehrabi@comp.iust.ac.ir>
2021-11-10 22:26:36 +0330

    login

---
+++ src/components/login.js
@@ -0,0 +1,150 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Empty from "./empty";
+import profile from "./profileDashboard";
+import signUp from "./signUp";
+import { Button, Modal } from "react-bootstrap";
+// import axios from "axios";
+
+class login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showHide: true,
+      user_name: null,
+      password: null,
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    // this.submit = this.submit.bind(this);
+  }
+  handleInputChange(event) {
+    const target = event.target;
+    let value = target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleModalShowHide() {
+    this.setState({ showHide: !this.state.showHide });
+  }
+
+  render() {
+    return (
+      <Router>
+        <div>
+          {/* <Button variant="primary" onClick={() => this.handleModalShowHide()}>
+          ثبت نام
+        </Button> */}
+
+          <Modal show={this.state.showHide}>
+            {/* <Modal.Header
+              closeButton
+              onClick={() => this.handleModalShowHide()}
+            >
+              <Modal.Title>ورود</Modal.Title>
+            </Modal.Header> */}
+            <Modal.Body>
+              <div className="align-items-right text-right">
+                <Link
+                  type="button"
+                  to={"/"}
+                  href="#"
+                  className="btn"
+                  onClick={() => this.handleModalShowHide()}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-arrow-right"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+                    />
+                  </svg>
+                  <Modal.Title></Modal.Title>
+                </Link>
+              </div>
+              <h4 className="text-center">ورود</h4>
+              <form className="text-right">
+                <div className="form-group">
+                  <label>نام کاربری</label>
+                  <input
+                    type="text"
+                    className="form-control text-right"
+                    name="user_name"
+                    onChange={this.handleInputChange}
+                    placeholder="نام کاربری خود را وارد کنید"
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label>رمز عبور</label>
+                  <input
+                    type="password"
+                    name="password"
+                    onChange={this.handleInputChange}
+                    className="form-control text-right"
+                    placeholder="رمز عبور خود را وارد کنید"
+                  />
+                </div>
+
+                <p className="forgot-password text-right">
+                  قبلا ثبت نام نکرده اید؟{" "}
+                  <Link
+                    className="nav-link"
+                    to={"/ثبت_نام"}
+                    href="#"
+                    onClick={() => {
+                      this.handleModalShowHide();
+                    }}
+                  >
+                    ثبت نام
+                  </Link>
+                </p>
+              </form>
+            </Modal.Body>
+            <Modal.Footer>
+              <Link
+                type="submit"
+                className="btn btn-primary"
+                to={"/پروفايل_كاربري"}
+                href="#"
+                onClick={() => {
+                  // this.submit();
+                  this.handleModalShowHide();
+                }}
+              >
+                ورود به سایت
+              </Link>
+              <Link
+                className="btn btn-secondary"
+                to={"/"}
+                href="#"
+                onClick={() => this.handleModalShowHide()}
+              >
+                خروج
+              </Link>
+            </Modal.Footer>
+          </Modal>
+          <div className="auth-wrapper">
+            <div className="auth-inner">
+              <Switch>
+                <Route exact path="/" component={Empty} />
+                <Route path="/ثبت_نام" component={signUp} />
+                <Route path="/پروفايل_كاربري" component={profile} />
+              </Switch>
+            </div>
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default login;
---
+++ src/components/signUp.js
@@ -0,0 +1,431 @@
+import React from "react";
+import { Button, Modal } from "react-bootstrap";
+// import { MDBRow, MDBCol, MDBBtn } from "mdbreact";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "../App.css";
+import Empty from "./empty";
+import login from "./login";
+// import axios from "axios";
+
+const regExp = RegExp(
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+);
+// const formValid = ({ isError, ...rest }) => {
+//   let isValid = false;
+
+//   Object.values(isError).forEach((val) => {
+//     if (val.length > 0) {
+//       isValid = false;
+//     } else {
+//       isValid = true;
+//     }
+//   });
+
+//   Object.values(rest).forEach((val) => {
+//     if (val === null) {
+//       isValid = false;
+//     } else {
+//       isValid = true;
+//     }
+//   });
+
+//   return isValid;
+// };
+class signUp extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      // isFormValid: true,
+      showHide: true,
+      showHideError: false,
+      // error: "",
+      first_name: "",
+      user_name: "",
+      email: "",
+      address: "",
+      bookOrPerson: "",
+      phone: "",
+      password: "",
+      isError: {
+        showHide: "",
+        first_name: "",
+        user_name: "",
+        email: "",
+        address: "",
+        bookOrPerson: "",
+        phone: "",
+        password: "",
+      },
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.submit = this.submit.bind(this);
+  }
+  handleInputChange = (e) => {
+    e.preventDefault();
+    // const target = event.target;
+    // let value = target.value;
+    // const name = target.name;
+    // this.setState({
+    //   [name]: value,
+    // });
+
+    const { name, value } = e.target;
+    let isError = { ...this.state.isError };
+
+    switch (name) {
+      case "first_name":
+        isError.first_name =
+          value.length < 1 ? "!اين فيلد نمي تواند خالي باشد" : "";
+        break;
+      case "user_name":
+        isError.user_name =
+          value.length < 1 ? "!اين فيلد نمي تواند خالي باشد" : "";
+        break;
+      case "address":
+        isError.address =
+          value.length < 1 ? "!اين فيلد نمي تواند خالي باشد" : "";
+        break;
+      case "phone":
+        isError.phone =
+          value.length < 11 || value.length > 11
+            ? "!تلفن همراه معتبر نيست"
+            : "";
+        break;
+      case "email":
+        isError.email = !regExp.test(value) ? "!آدرس ايميل معتبر نيست" : "";
+        if (value.length < 1) {
+          isError.email = "!اين فيلد نمي تواند خالي باشد";
+        }
+        break;
+      case "password":
+        isError.password =
+          value.length < 6 ? "!رمز عبور انتخابي خيلي كوتاه است" : "";
+        break;
+      default:
+        break;
+    }
+
+    this.setState({
+      isError,
+      [name]: value,
+    });
+    console.log(this.state);
+  };
+
+  submit() {
+    let isFormValid = true;
+    Object.values(this.state.isError).forEach((element) => {
+      if (element.length > 0) {
+        isFormValid = false;
+      }
+    });
+    if (isFormValid === true) {
+      if (this.state.address.length < 1) {
+        isFormValid = false;
+      }
+      if (this.state.password.length < 1) {
+        isFormValid = false;
+      }
+      if (this.state.phone.length < 1) {
+        isFormValid = false;
+      }
+      if (this.state.email.length < 1) {
+        isFormValid = false;
+      }
+      if (this.state.user_name.length < 1) {
+        isFormValid = false;
+      }
+      if (this.state.first_name.length < 1) {
+        isFormValid = false;
+      }
+    }
+    if (isFormValid === true) {
+      console.log(this.state);
+      this.handleModalShowHide();
+    } else {
+      this.handleModalShowHideError();
+      console.log(this.state);
+    }
+  }
+  handleModalShowHide() {
+    this.setState({ showHide: !this.state.showHide });
+  }
+  handleModalShowHideError() {
+    this.setState({ showHideError: !this.state.showHideError });
+  }
+
+  render() {
+    const { isError } = this.state;
+    return (
+      <Router>
+        <div>
+          <Modal show={this.state.showHide}>
+            <Modal.Body>
+              <div className="align-items-right text-right">
+                <Link
+                  type="button"
+                  to={"/"}
+                  href="#"
+                  className="btn"
+                  onClick={() => this.handleModalShowHide()}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-arrow-right"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+                    />
+                  </svg>
+                  <Modal.Title></Modal.Title>
+                </Link>
+              </div>
+              <h4 className="text-center">ثبت نام</h4>
+              <form className="text-right" noValidate>
+                <div className="form-group">
+                  <label htmlFor="full_name">نام</label>
+                  <input
+                    type="text"
+                    className={
+                      isError.first_name.length > 0
+                        ? "is-invalid form-control text-right"
+                        : "form-control text-right"
+                    }
+                    name="first_name"
+                    onChange={this.handleInputChange}
+                    placeholder="لطفا نام شخص و یا کتابفروشی را وارد کنید"
+                  />
+                  <small className="text-danger">
+                    {isError.first_name.length > 0 ? isError.first_name : ""}
+                  </small>
+                </div>
+
+                <div className="form-group">
+                  <label>نام کاربری</label>
+                  <input
+                    type="text"
+                    className={
+                      isError.user_name.length > 0
+                        ? "is-invalid form-control text-right"
+                        : "form-control text-right"
+                    }
+                    name="user_name"
+                    onChange={this.handleInputChange}
+                    placeholder="لطفا یک نام کاربری برای خود انتخاب کنید"
+                  />
+                  <small className="text-danger">
+                    {isError.user_name.length > 0 ? isError.user_name : ""}
+                  </small>
+                </div>
+
+                <div className="form-group">
+                  <label>پست الکترونیکی</label>
+                  <input
+                    type="email"
+                    className={
+                      isError.email.length > 0
+                        ? "is-invalid form-control text-right"
+                        : "form-control text-right"
+                    }
+                    name="email"
+                    onChange={this.handleInputChange}
+                    placeholder="لطفا آدرس پست الکترونیکی خود را وارد کنید"
+                  />
+                  <small className="text-danger">
+                    {isError.email.length > 0 ? isError.email : ""}
+                  </small>
+                </div>
+
+                <div className="form-group">
+                  <label>آدرس</label>
+                  <input
+                    type="text"
+                    name="address"
+                    onChange={this.handleInputChange}
+                    className={
+                      isError.address.length > 0
+                        ? "is-invalid form-control text-right"
+                        : "form-control text-right"
+                    }
+                    placeholder="لطفا آدرس شخص و یا کتابفروشی را وارد کنید"
+                  />
+                  <small className="text-danger">
+                    {isError.address.length > 0 ? isError.address : ""}
+                  </small>
+                </div>
+
+                <div className="form-group">
+                  <label>تلفن همراه</label>
+                  <input
+                    type="phone"
+                    className={
+                      isError.phone.length > 0
+                        ? "is-invalid form-control text-right"
+                        : "form-control text-right"
+                    }
+                    name="phone"
+                    onChange={this.handleInputChange}
+                    placeholder="لطفا شماره تماس شخص و یا کتابفروشی را وارد کنید"
+                  />
+                  <small className="text-danger">
+                    {isError.phone.length > 0 ? isError.phone : ""}
+                  </small>
+                </div>
+
+                <div className="form-group">
+                  <label>رمز عبور</label>
+                  <input
+                    type="password"
+                    name="password"
+                    onChange={this.handleInputChange}
+                    className={
+                      isError.password.length > 0
+                        ? "is-invalid form-control text-right"
+                        : "form-control text-right"
+                    }
+                    placeholder="لطفا یک رمز عبور قوی برای حساب خود انتخاب کنید"
+                  />
+                  <small className="text-danger">
+                    {isError.password.length > 0 ? isError.password : ""}
+                  </small>
+                </div>
+
+                <div className="form-group">
+                  <div>
+                    <label>:نوع حساب كاربري خود را انتخاب كنيد</label>
+                  </div>
+                  <div className="form-check form-check-inline">
+                    <input
+                      className="form-check-input"
+                      type="radio"
+                      name="bookOrPerson"
+                      id="inlineRadio1"
+                      value="library"
+                      checked={this.state.bookOrPerson === "library"}
+                      onChange={this.handleInputChange}
+                    />
+                    <label className="form-check-label" htmlFor="inlineRadio1">
+                      كتابفروشي
+                    </label>
+                  </div>
+                  <div className="form-check form-check-inline">
+                    <input
+                      className="form-check-input"
+                      type="radio"
+                      name="bookOrPerson"
+                      id="inlineRadio2"
+                      value="person"
+                      checked={this.state.bookOrPerson === "person"}
+                      onChange={this.handleInputChange}
+                    />
+                    <label className="form-check-label" htmlFor="inlineRadio2">
+                      شخص حقيقي
+                    </label>
+                  </div>
+                </div>
+
+                <p className="forgot-password text-right">
+                  قبلا ثبت نام کرده اید؟{" "}
+                  <Link
+                    className="nav-link"
+                    to={"/ورود"}
+                    href="#"
+                    onClick={() => {
+                      this.handleModalShowHide();
+                    }}
+                  >
+                    ورود
+                  </Link>
+                </p>
+              </form>
+            </Modal.Body>
+            <Modal.Footer>
+              <Link
+                type="submit"
+                className="btn btn-primary"
+                to={"/"}
+                href="#"
+                onClick={() => {
+                  this.submit();
+                }}
+              >
+                ذخیره تغییرات
+              </Link>
+              <Link
+                to={"/"}
+                href="#"
+                className="btn btn-secondary"
+                onClick={() => this.handleModalShowHide()}
+              >
+                خروج
+              </Link>
+            </Modal.Footer>
+          </Modal>
+          <Modal show={this.state.showHideError}>
+            <div className="text-center">
+              {/* <Modal.Header
+              closeButton
+              onClick={() => this.handleModalShowHideError()}
+            >
+              <Modal.Title></Modal.Title>
+            </Modal.Header> */}
+              <Modal.Body>
+                <div className="align-items-right text-right">
+                  <button
+                    type="button"
+                    className="btn"
+                    onClick={() => this.handleModalShowHideError()}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className="bi bi-arrow-right"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+                      />
+                    </svg>
+                    <Modal.Title></Modal.Title>
+                  </button>
+                </div>
+                <div className="text-center">
+                  <h5>شما فرم را به درستی تکمیل نکرده اید</h5>
+                  <h5>پر کردن همه فیلد ها اجباری می باشد</h5>
+                </div>
+              </Modal.Body>
+              <Modal.Footer>
+                <button
+                  type="button"
+                  className="btn btn-secondary btn-lg btn-container-left btn-block"
+                  onClick={() => this.handleModalShowHideError()}
+                >
+                  فهمیدم
+                </button>
+              </Modal.Footer>
+            </div>
+          </Modal>
+
+          <div className="auth-wrapper">
+            <div className="auth-inner">
+              <Switch>
+                <Route exact path="/" component={Empty} />
+                <Route path="/ورود" component={login} />
+              </Switch>
+            </div>
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default signUp;
