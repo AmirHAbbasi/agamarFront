511b86aa9113ea866009e21bb74f8dbc4947db05
Hossein <ah.rahimi19219@gmail.com>
2021-11-30 15:51:11 +0330

    edit

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -7,6 +7,9 @@ const Banner = () => {
   const contentStyle = {
       height: '200px',
       color: '#000000',
+      //backgroundImage:"attr(im)",
+      //backgroundRepeat: 'no-repeat',
+      //backgroundSize: '100% 100%',
       lineHeight: '160px',
       textAlign: 'center',
       background: '#FC74B5',
@@ -15,7 +18,7 @@ const Banner = () => {
     <section>
       <Carousel autoplay>
         <div>
-          <h3 style={contentStyle}>بزرگترین مرجع اشتراک گذاری کتاب</h3>
+          <h3 style={contentStyle }>بزرگترین مرجع اشتراک گذاری کتاب</h3>
         </div>
         <div>
           <h3 style={contentStyle}>فروش کتاب های شما</h3>
