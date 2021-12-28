2df2a78e2be22955552377f9480c0c3f75f6ed7b
Sahar Mehrabi <s_mehrabi@comp.iust.ac.ir>
2021-12-28 21:50:08 +0330

    add the page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 import React from 'react'
+
+import Login from "./components/login/login";
+import Profile from "./components/profile/profile";
+import AddBook from "./components/AddBook/AddBook";
+import SignUp from "./components/signUp/signUp";
+
+
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import BookList from './components/BookList/BookList'
 import Banner from './components/Banner/Banner'
@@ -7,78 +14,95 @@ import { AppProvider } from './ReqBookList'
 import Navbarr from './components/Navbar/navbarr'
 import Footter from './components/Footer/footer2'
 import Antdmodal from "./components/AdvancedSearch/antdmodal";
+
+
+
+
 import './App.css'
 
 class App extends React.Component {
-    
+
   constructor(props) {
-      super(props);
-      this.serverAddress = 'http://127.0.0.1:8000/';  // Hossein Rahimi, Mehrabi : change all requests
-      this.navbar = React.createRef();
-      this.loginModal = React.createRef();    // Mehrabi
-      this.regModal = React.createRef();     // Mehrabi
-      this.bookCards = React.createRef();    // Hossein Rahimi
-      this.categories = ["علمی","داستانی","درسی","انگلیسی"]
-    }
-
-  
-
-
-  showResult = (results) =>{        
-      // Show Results : Hossein Rahimi
-      // results : backend response
-      
+    super(props);
+    this.serverAddress = 'http://127.0.0.1:8000';  // Hossein Rahimi, Mehrabi : change all requests
+    this.navbar = React.createRef();
+    this.LoginModal = React.createRef();    // Mehrabi
+    this.regModal = React.createRef();     // Mehrabi
+    this.bookCards = React.createRef();    // Hossein Rahimi
+    this.categories = ["علمی", "داستانی", "درسی", "انگلیسی"]
   }
 
-  handleRegister = () =>{
-      // Show sign up modal   :   Mehrabi
-      // this.setState({username: []  });       change this.state.username
-      // this.submitLoginRegister(); at the end if login submitted, call this.submitLoginRegister function
+
+
+
+  showResult = (results) => {
+
+    const bl = this.bookCards.current.props.children[0].props.children[3].props.children;
+    console.log(bl);
+    bl.showResults(results);
+    // Show Results : Hossein Rahimi
+    // results : backend response
+
   }
-  handleLogin = () =>{
-      // Show login modal     :   Mehrabi  
-      // this.submitLoginRegister(data); at the end if login submitted, call this.submitLoginRegister function
-      //your data must be like this :
-      /*
-      user_info = {
-         username : "Amir_abbasi_77",
-         name : "Amirhossein",
-         lname :  "Abbasi",
-      }
-      */
+
+  handleRegister = () => {
+    this.regModal.current.handleModalShowHide();
+    // Show sign up modal   :   Mehrabi
+    // this.setState({username: []  });       change this.state.username
+    // this.submitLoginRegister(); at the end if login submitted, call this.submitLoginRegister function
+  }
+  handleLogin = () => {
+    this.LoginModal.current.handleModalShowHide();
+
   }
   submitLoginRegister = (user_info) => {
-      /*
-      user_info = {
-         username : "Amir_abbasi_77",
-         name : "Amirhossein",
-         lname :  "Abbasi",
-      }
-      */
-      this.navbar.current.toggleNavBar(user_info);
-  }
-  render() { 
+    /*
+    user_info = {
+       username : "Amir_abbasi_77",
+       name : "Amirhossein",
+       lname :  "Abbasi",
+    }
+    */
+    this.user_info = user_info;
+    console.log(this.navbar)
+    this.navbar.current.toggleNavBar(user_info);
 
-  return (  
-            <Router>
-              <Navbarr/>
-              <br /><br /><br /><br />
-                                   
-              <Switch>
-                <Route exact path = "/">
-                  <Banner/>
-                  <Antdmodal  serverAddress={this.serverAddress} onResult={(e) => {this.showResult(e)}}  vis={false} categories={this.categories} />
-                  <FilterIcon/>
-                  <AppProvider> 
-                    <BookList/>
-                  </AppProvider>
-                </Route>
-              </Switch>
-              <br /><br /><br /><br />
-              <Footter/>
-            </Router>
-            
-  );}
+  }
+
+  submitt = () => {
+    // this.LoginModal.current.handleModalShowHide();
+  }
+  render() {
+
+    return (
+      <Router>
+        <Navbarr serverAddress={this.serverAddress} ref={this.navbar} handleRegister={this.handleRegister} handleLogin={this.handleLogin} />
+        <br /><br /><br /><br />
+        <Login dir="rtl" serverAddress={this.serverAdress} ref={this.LoginModal} onSubmit={(user_info) => { this.submitLoginRegister(user_info) }} />
+        <SignUp dir="rtl" serverAddress={this.serverAdress} submit={this.submitt} ref={this.regModal} onSubmit={(user_info) => { this.submitLoginRegister(user_info) }} />
+
+        <Switch ref={this.bookCards}>
+          <Route exact path="/">
+            <Banner />
+            <Antdmodal serverAddress={this.serverAddress} onResult={(e) => { this.showResult(e) }} vis={false} categories={this.categories} />
+            <FilterIcon />
+            <AppProvider>
+              <BookList serverAddress={this.serverAddress} ref={this.bookCards} />
+            </AppProvider>
+          </Route>
+          <Route exact path="/profile">
+            <Profile dir="rtl" />
+          </Route>
+          <Route exact path="/addbook">
+            <AddBook dir="rtl" />
+          </Route>
+        </Switch>
+        <br /><br /><br /><br />
+        <Footter />
+      </Router>
+
+    );
+  }
 }
 
 
--- src/components/AdvancedSearch/autocomplete.jsx
+++ src/components/AdvancedSearch/autocomplete.jsx
@@ -7,51 +7,56 @@ import axios from 'axios';
 class Complete extends React.Component {
 
   state = {
-    options : []
+    options: []
   };
 
-  
 
-  
+
+
   updateAut = (text) => {
-    
-    if(text==""){this.setState({options:[]})
-                return 0;
-                }
 
-    
+    if (text == "") {
+      this.setState({ options: [] })
+      return 0;
+    }
+
+
 
     const ref = this;
-    
-    axios.get(this.props.serverAddress+"api/book-find/"+text+"/")
-    .then(function (response) {
-      console.log(response)
-      var ops = response.data.map(o=>{return {value:o.title, label:
-        (
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              /*backgroundColor:"rgba(255, 90, 169,0.1)",*/
-              color:"rgb(255, 90, 169)"
-            }}
-          >
-            <span dir="rtl" style={{maxWidth:'50%',
-                         textAlign:"left",
-                         }} >
-              <a>
-                {o.author}
-              </a>
-            </span>
-            <span style={{maxWidth:'50%', textAlign:"right"}}><a dir="rtl">{o.title}</a></span>
-          </div>
-        )
-      }})
-      ref.setState({options:ops})
-    })
-    .catch(function (error) {
-      console.log("error:"+error);
-    });
+
+    axios.get(this.props.serverAddress + "/api/book-find/" + text + "/")
+      .then(function (response) {
+        console.log(response)
+        var ops = response.data.map(o => {
+          return {
+            value: o.title, label:
+              (
+                <div
+                  style={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    /*backgroundColor:"rgba(255, 90, 169,0.1)",*/
+                    color: "rgb(255, 90, 169)"
+                  }}
+                >
+                  <span dir="rtl" style={{
+                    maxWidth: '50%',
+                    textAlign: "left",
+                  }} >
+                    <a>
+                      {o.author}
+                    </a>
+                  </span>
+                  <span style={{ maxWidth: '50%', textAlign: "right" }}><a dir="rtl">{o.title}</a></span>
+                </div>
+              )
+          }
+        })
+        ref.setState({ options: ops })
+      })
+      .catch(function (error) {
+        console.log("error:" + error);
+      });
 
     /*
     axios.post(this.props.serverAddress+"api/autocomplete", {
@@ -75,25 +80,26 @@ class Complete extends React.Component {
 
   };
 
-  render() { 
-    
-  
+  render() {
+
+
     return <>
-            <AutoComplete dropdownClassName="autocompletedrpdwn"
-              dir="rtl"
-              options={this.state.options}
-              style={{
-                width: "70%", color:'rgba(255, 90, 169)'
-              }}  
-              backgroundColor= "lightblue"
-              onChange={(e) => {this.updateAut(e);this.props.onChange(e);}}
-              
-              placeholder="از میان بیش از 1 میلیون کتاب جستجو کنید!"
-            >
-              
-            </AutoComplete>
-          </>;
+      <AutoComplete dropdownClassName="autocompletedrpdwn"
+        dir="rtl"
+        options={this.state.options}
+        style={{
+          width: "70%", color: 'rgba(255, 90, 169)'
+        }}
+        backgroundColor="lightblue"
+        onChange={(e) => { this.updateAut(e); this.props.onChange(e); }}
+
+        placeholder="از میان بیش از 1 میلیون کتاب جستجو کنید!"
+
+      >
+
+      </AutoComplete>
+    </>;
   }
 }
- 
+
 export default Complete;
